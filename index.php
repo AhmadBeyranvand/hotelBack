9463f295c51deb550516938a1072a315811d27ad
Ahmad Beyranvand <ahmadb1881@gmail.com>
2023-06-12 21:26:06 +0330

    created dynamic Route

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
+require("./config.php");
 $url = explode("/hotelBack/",$_SERVER['REQUEST_URI']);
+
+
+
+
 
 $route = [
     "allHotels" => "./api/getAllHotels.php",
     "hotelBelowPrice" => "./api/getHotelsBelowPrice.php"
 ];
-// echo $url[1];
+
+
+
+
+
 for($i=0; $i<sizeof(array_keys($route)); $i++ ) {
     if( $url[1] == array_keys($route)[$i]){
         require( array_values($route)[$i] );
